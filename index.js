f3ac2c0eff2baa534690d5715e5a29dd39c133fd
suzitsarkar27 <96899998+suzitsarkar27@users.noreply.github.com>
2022-05-07 20:16:53 +0600

    data-connect

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middel aware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ifo2k.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const servicecollection = client.db("data").collection("service");
+
+    app.get("/data", async (req, res) => {
+      const query = {};
+      const cursor = servicecollection.find(query);
+      const service = await cursor.toArray();
+      res.send(service);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("BOOK INVENTORY CRUD IS RUNNING");
+});
+app.listen(port, () => {
+  console.log("LISTING CRUD IS RUNNING", port);
+});
